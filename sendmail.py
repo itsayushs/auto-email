9bf66325323a27022671612bb63be49c450fc58d
ayushl33t <ayushstarksharma@gmail.com>
2018-02-04 15:32:53 +0530

    Error handling added

--- sendmail.py
+++ sendmail.py
@@ -1,23 +1,32 @@
 # -*- coding: utf-8 -*-
 #use this pre installed library to send emails 
 
-import smtplib 
+
+from smtplib import SMTP, SMTPConnectError, SMTPAuthenticationError, SMTPException
 host= "smtp.gmail.com"
 port= 587
-username="email@email.com"
-password="########"
+username="xxx@gmail.com"
+password="xxx"
 
 
-fromu="email@email.com"
-tou="email@email.com"
-email_conn=smtplib.SMTP(host,port)
-#check if the connection is established or not
-email_conn.ehlo()
-#provides extra security layer or encryption to emails
-email_conn.starttls()
-#to login
-email_conn.login(username,password)
-#to send email
-email_conn.sendmail(fromu,tou,"hi its a python based email")
+fromu="xxx@gmail.com"
+tou="xxx@gmail.com"
+try:
+    email_conn=SMTP(host,port)
+    #check if the connection is established or not
+    email_conn.ehlo()
+    #provides extra security layer or encryption to emails
+    email_conn.starttls()
+    #to login
+    email_conn.login(username,password)
+    #to send email
+    email_conn.sendmail(fromu,tou,"hi its a python based email")
+except SMTPConnectError:
+    print ("Error while connecting check internet or host port details")
+except SMTPAuthenticationError:
+    print ("Error while authenticating login credentials")
+except :
+    print ("Woopsy! Try Again Later")
+
 #to logout form the server
 email_conn.quit()
